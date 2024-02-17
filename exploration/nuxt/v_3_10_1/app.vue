6cca3092b3666b44accc01dfb05da1ca1ac2accc
Eugene Kosukhin <kosuha606@gmail.com>
2024-02-17 10:19:59 +0300

    feat: Последнее

---
+++ exploration/nuxt/v_3_10_1/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>
+      <NuxtLink to="/">
+        Home
+      </NuxtLink>
+      <NuxtLink to="/one">
+        One
+      </NuxtLink>
+      <NuxtLink to="/two">
+        Two
+      </NuxtLink>
+    </div>
+    <NuxtPage />
+  </div>
+</template>
---
+++ exploration/nuxt/v_3_10_1/pages/one.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        one page
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            const route = useRoute()
+            console.log(route);
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
