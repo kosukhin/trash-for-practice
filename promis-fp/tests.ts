ed68898ad7a00c86ddb1d20dfd57395c7ecb4786
Eugene Kosukhin <kosuha606@gmail.com>
2024-03-03 11:37:38 +0300

    feat: Рефакторинг

--- promis-fp/tests.ts
+++ promis-fp/tests.ts
@@ -1,5 +1,39 @@
 import { pipe } from "./pipe";
 
+setTimeout(() =>
+  commonHandler(pipe(debug("\nУспешно"), pass("turtle"), toUpper))
+);
+
+setTimeout(() =>
+  commonHandler(
+    pipe(
+      debug("\nОшибка"),
+      pass("string"),
+      checkNotEquals("string"),
+      toUpper,
+      debug("Этот код не вызывается")
+    )
+  )
+);
+
+setTimeout(() =>
+  commonHandler(
+    pipe(
+      debug("\nМассивы"),
+      pass(["one", "two"]),
+      tap((v: any) => console.log("тип аргумента", typeof v)),
+      debug("Перед объединением = %v"),
+      join
+    )
+  )
+);
+
+const commonHandler = (v: Promise<unknown>) =>
+  v
+    .then(debug("result = %v"))
+    .finally(debug("finally do smth!"))
+    .catch(debug("catch = %v"));
+
 const pass = (v: any) => () => v;
 
 const tap = (fn: Function) => (v: any) => {
@@ -14,31 +48,7 @@ const checkNotEquals = (possibleV: any) => (v: any) =>
 
 const join = (v: string[]) => v.join(" ");
 
-const debug = (message: string) => (v: any) => {
+const debug = (message: string) => (v?: any) => {
   console.log(message.replace("%v", JSON.stringify(v)));
   return v;
 };
-
-setTimeout(() =>
-  pipe(debug("\nУспешно"), pass("turtle"), toUpper)
-    .then(debug("result = %v"))
-    .catch(debug("catch = %v"))
-);
-
-setTimeout(() =>
-  pipe(debug("\nОшибка"), pass("string"), checkNotEquals("string"), toUpper)
-    .then(debug("result = %v"))
-    .catch(debug("catch = %v"))
-);
-
-setTimeout(() =>
-  pipe(
-    debug("\nМассивы"),
-    pass(["one", "two"]),
-    tap((v: any) => console.log("тип аргумента", typeof v)),
-    debug("Перед объединением = %v"),
-    join
-  )
-    .then(debug("result = %v"))
-    .catch(debug("catch = %v"))
-);
